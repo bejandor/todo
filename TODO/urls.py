e5674cff1580abdc601055a3be340d9722932adb
Bektemir <bejandor@gmail.com>
2021-05-13 12:26:14 +0300

    Added the the third test page

--- TODO/urls.py
+++ TODO/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
-from main.views import test,second
+from main.views import test,second,third
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",homepage,name="home"),
     path("test/", test, name="test"),
-    path("test2/",second)
+    path("test2/",second),
+    path("test3/",third)
 ]
--- main/views.py
+++ main/views.py
@@ -10,4 +10,7 @@ def test(request):
     return render(request,"test.html")
 
 def second(request):
-        return HttpResponse("Test2 page")
+    return HttpResponse("Test2 page")
+
+def third(request):
+    return HttpResponse("This is page test3")
