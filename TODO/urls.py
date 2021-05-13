7d2bcafef4d70e8eca54bda67a2d9c2f4acab510
Bektemir <bejandor@gmail.com>
2021-05-13 12:06:05 +0300

    added second test page and url

--- TODO/urls.py
+++ TODO/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
-from main.views import test
+from main.views import test,second
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",homepage,name="home"),
-    path("test/", test, name="test")
+    path("test/", test, name="test"),
+    path("test2/",second)
 ]
--- main/views.py
+++ main/views.py
@@ -8,3 +8,6 @@ def homepage(request):
 
 def test(request):
     return render(request,"test.html")
+
+def second(request):
+        return HttpResponse("Test2 page")
